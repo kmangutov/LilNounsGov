f3626f12add5c2910cca67595650031e8661f4fc
kmangutov <kmangutov@gmail.com>
2022-07-22 10:23:57 -0700

    add prop house

--- src/SourcePropHouse.ts
+++ src/SourcePropHouse.ts
@@ -1,1 +1,23 @@
 // Scrape https://prop.house/lil-nouns
+
+
+const PROPHOUSE_URL = 'https://prod.backend.prop.house/communities/name/lil%20nouns'
+
+class SourcePropHouse {
+
+	public download(cb) {
+		fetch(<any>PROPHOUSE_URL, {
+			  method: "GET",
+			  headers: {
+			    "Accept": "application/json"
+			  }
+		})
+		.then(res => res.text())
+		.then(res => console.log('res: ' + res))
+		// TODO Parse HTML with cheerio to get text, descriptions
+
+		return "Hello World Source";
+	}
+}
+
+export = SourcePropHouse;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 
 import SourceSnapshot = require('./SourceSnapshot');
 import SourceDiscourse = require('./SourceDiscourse');
+import SourceProposals = require('./SourceProposals');
+import SourcePropHouse = require('./SourcePropHouse');
+
 
 console.log('hello');
 
@@ -8,8 +11,18 @@ const snapshot = new SourceSnapshot()
 snapshot.download((str) => console.log(str))
 
 const discourse = new SourceDiscourse()
-discourse.download((str) => console.log(str)) 
+discourse.download((str) => console.log(str))
+
+
+const proposals = new SourceProposals()
+proposals.download((str) => console.log(str))
+
+
+const prophouse = new SourcePropHouse()
+prophouse.download((str) => console.log(str))
+
 
 // TODO: Merge data sources and save into csv
+// Data structure [{title: '', description: '', created: DateTime, type: Discourse/PropHouse/Proposals/Snapshot}]
 // TODO: Simple dashboard use data from csv
 // TODO: Use await/async to avoid callbacks
