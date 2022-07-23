ac0f481b1ed5ca2085f3d3ffcc497cbbe8115f32
kmangutov <kmangutov@gmail.com>
2022-07-23 01:39:36 -0700

    ui0

--- web/index.js
+++ web/index.js
@@ -1,83 +1,45 @@
 
-var proposals = []
-
 url = "./output.csv"
 $.get(url, function(data) {
 	console.log('data: ' + data)
-
-	proposals = [
-  {
-    id: 1,
-    name: 'James',
-    handle: '@jokerjames',
-    img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
-    tweet: "If you don't succeed, dust yourself off and try again.",
-    likes: 10,
-  },
-  { 
-    id: 2,
-    name: 'Fatima',
-    handle: '@fantasticfatima',
-    img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
-    tweet: 'Better late than never but never late is better.',
-    likes: 12,
-  },
-  {
-    id: 3,
-    name: 'Xin',
-    handle: '@xeroxin',
-    img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
-    tweet: 'Beauty in the struggle, ugliness in the success.',
-    likes: 18,
-  }
-]
-
 })
 
+var itemsVue = new Vue({
+	el: '#app',
+	data: {
+		text: 'placeholder',
+		proposals: []
+	},
+	mounted: function () {
+		var self = this
+		url = "./output.csv"
+		$.get(url, function(data) {
+			// console.log('data: ' + data)
+			self.text = "" + data
+			var items = data.split('\n')
+			var finalItems = []
 
+			for(var item of items) {
+				var itemsArr = item.split(',')
+				console.log(itemsArr[0])
 
-Vue.component('proposal-component', {
-	template: `
-	<div>
-		{{proposal.tweet}}
-	</div>`,
-  /*template: `
-    <div class="tweet">
-      <div class="box">
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <img :src="tweet.img" alt="Image">
-            </figure>
-          </div>
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong>{{tweet.name}}</strong> <small>{{tweet.handle}}</small>
-                <br>
-                {{tweet.tweet}}
-              </p>
-            </div>
-              <div class="level-left">
-                <a class="level-item">
-                  <span class="icon is-small"><i class="fas fa-heart"></i></span>
-                  <span class="likes">{{tweet.likes}}</span>
-                </a>
-              </div>
-          </div>
-        </article>
-      </div>
-    </div> 
-  `,*/
-  // data: {}
-  props: {
-    proposal: Object
-  }
-});
+				var timestampHuman =
+					new Date(1000 * itemsArr[0]).toDateString() + ' ' +
+					new Date(1000 * itemsArr[0]).toTimeString() 
 
-new Vue({
-  el: '#app',
-  data: {
-    proposals
-  }
+				var obj = {
+					timestamp: itemsArr[0],
+					timestampHuman: timestampHuman, //new Date(1000 * itemsArr[0]).toDateString(),
+					title: itemsArr[3],
+					url: itemsArr[2]
+				}
+				finalItems.push(obj)
+			}
+
+			finalItems.sort(function (a,b) {
+				return b.timestamp - a.timestamp
+			})
+			self.proposals = finalItems
+		})
+	}
 });
